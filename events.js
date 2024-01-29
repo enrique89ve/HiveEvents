3b8e4960ace901bc245df5453e55ff4a9da80b94
Enrique Vee <40583307+enrique89ve@users.noreply.github.com>
2024-01-29 16:27:09 -0400

    Create events.js

---
+++ events.js
@@ -0,0 +1,35 @@
+[
+    {
+        "id": "1",
+        "date": "17-01-2024",
+        "title": "MasterClass Conociendo HIVE",
+        "organizer": "Hive Community",
+        "image": "/events/images/masterclass.jpg",
+        "social": "colledge.social",
+        "url": "https://www.colledge.social/enrol/index.php?id=105",
+        "tipo": "En linea",
+        "body": "Un evento donde puedes aprender sobre Hive como blockchain"
+    },
+    {
+        "id": "2",
+        "date": "22-01-2024",
+        "title": "Organiza tus proyectos con Notion",
+        "organizer": "Hive Coity",
+        "image": "/events/images/notion.png",
+        "social": "Telegram",
+        "url": "https://t.me/hiveblockes",
+        "tipo": "En linea",
+        "body": "Organiza tus ideas, proyectos, contenido en un solo lugar, usando la herramienta Notionddd."
+    },
+    {
+        "id": "3",
+        "date": "29-02-2024",
+        "title": "Cómo desarrollar tus ideas en Hive",
+        "organizer": "Hive Creators",
+        "image": "/events/images/enrique89hive.png",
+        "social": "Telegram",
+        "url": "https://t.me/hiveblockes",
+        "tipo": "En linea",
+        "body": "Descubre como puedes desarrollar tus ideas en Hive, desde probar la idea hasta crear un MVP"
+    }
+]
